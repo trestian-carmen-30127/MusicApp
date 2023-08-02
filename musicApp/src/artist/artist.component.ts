02d729b0480a645fd515337bd7a8c7817b89ee39
trestian-carmen-30127 <99726661+trestian-carmen-30127@users.noreply.github.com>
2023-08-02 13:42:42 +0300

    cautare si afisare artisti

--- musicApp/src/artist/artist.component.ts
+++ musicApp/src/artist/artist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DeezerApiService } from '../deezer-api.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-artist',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class ArtistComponent {
 
+  searchQuery: string = '';
+  artist: any[] = [];
+
+  constructor(private deezerApiService: DeezerApiService, private router: Router) { }
+
+  searchArtists(): void {
+    if (this.searchQuery.trim() == '') {
+      this.artist = [];
+      return;
+    }
+
+    this.deezerApiService.searchArtists(this.searchQuery).subscribe(
+      (respone) => { this.artist = respone.data; },
+      (error) => { console.error('Error fetching artists:', error); }
+    );
+
+
+  }
+  goToAlbumList(artistId: number): void {
+    this.router.navigate(['/albums', artistId]);
+  }
 }
